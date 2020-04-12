a7d72eeb692f10feba394d4ae19a9f5a66e19826
Duncan Smith <duncan@dubsado.com>
2020-04-11 19:43:32 -0700

    Use nacl.hash instead of separate lib

--- envelope.ts
+++ envelope.ts
@@ -1,6 +1,5 @@
 import { Buffer } from 'buffer/'
 import nacl from 'tweetnacl'
-import { sha512 } from 'js-sha512'
 
 type IdentityKeyPair = nacl.SignKeyPair
 
@@ -17,16 +16,14 @@ export class Envelope {
   }
 
   public sign(ikp: IdentityKeyPair) {
-    const hash = sha512.create()
-    hash.update(this.message)
-    this.signature = Buffer.from(nacl.sign.detached(Buffer.from(hash.array()), ikp.secretKey))
+    const hash = nacl.hash(this.message)
+    this.signature = Buffer.from(nacl.sign.detached(hash, ikp.secretKey))
     this.author = Buffer.from(ikp.publicKey)
   }
 
   public verify() {
-    const hash = sha512.create()
-    hash.update(this.message)
-    return nacl.sign.detached.verify(Buffer.from(hash.array()), this.signature, this.author)
+    const hash = nacl.hash(this.message)
+    return nacl.sign.detached.verify(hash, this.signature, this.author)
   }
 
   static parse(str: string): Envelope {
